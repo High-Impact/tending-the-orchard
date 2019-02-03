9f7c54539b8d65f3803d4b36767e0a54f60ddf42
Jonathan Knoll <jknoll@highimpact.design>
2019-02-03 13:17:22 -0600

    navigation > Navigation

--- shared/Header.js
+++ shared/Header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Navigation from './navigation'
+import Navigation from './Navigation'
 import MobileNavigation from './MobileNavigation'
 
 const Header = (props) => (
